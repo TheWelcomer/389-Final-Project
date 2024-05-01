1dfe10061d545006f7960fa3947baa3c725b0d71
Philip Phan <phantriphuoc@yahoo.com>
2024-05-01 19:03:33 -0400

    adding small detail changes

--- video_to_frames.py
+++ video_to_frames.py
@@ -56,8 +56,9 @@ def frames_to_video(input_folder, video_path=""):
         video.write(cv2.imread(imgs[i]))
         #print ("frame ", imgs[i])
     video.release()
+    print ("\nVideo released")
 
-def create_labeled_frames(input_folder, text_labels, output_folder, new_filename="labeled_"):
+def create_labeled_frames(input_folder, text_labels, output_folder, new_filename="labeled_", **kwargs):
     imgs = []
     print ("Fetching image filenames:")
     for filename in tqdm(os.listdir(input_folder)):
@@ -68,27 +69,31 @@ def create_labeled_frames(input_folder, text_labels, output_folder, new_filename
     if not len(imgs) == len(text_labels):
         raise AssertionError("Number of images not same as number of labels")
 
-    my_font = ImageFont.truetype("arial.ttf", 30)
+    my_font = ImageFont.truetype("arial.ttf" if not "font" in kwargs else kwargs["font"], 
+                                 30 if not "font_size" in kwargs else kwargs["font_size"])
 
-    #img.save(os.path.join(output_folder, new_filename + imgs[i]))
+    if not os.path.exists(output_folder):
+        os.mkdir(output_folder)
 
     print ("Writing new frames:")
     for i in tqdm(range(len(imgs))):
         img = Image.open(os.path.join(input_folder, imgs[i]))
         draw = ImageDraw.Draw(img)
-        draw.text((100, 100), text_labels[i], align="center", font=my_font, fill=(255, 0, 0))
+        draw.text((100, 100) if not "position" in kwargs else kwargs["position"], 
+                  text_labels[i], align="center", font=my_font, fill=(255, 0, 0) if not "color" in kwargs else kwargs["color"])
         img.save(os.path.join(output_folder, new_filename + imgs[i]))
+    print ("\nFinished writing frames")
 
-frames_to_video("./test_output", "./videos/test_labeled.mp4")
 
 #frames_to_video("./vid0_frames", "./videos/test1.mp4")
 
 labels = []
 with open("./labeled/0.txt") as f:
     lines = f.readlines()
-    for line in lines:
+    for i, line in enumerate(lines):
         data = line.split(" ")
         new_label = "pitch: " + data[0] + ", yaw: " + data[1]
         labels.append(new_label)
 
-#create_labeled_frames("./vid0_frames", labels, "./test_output")
+""" create_labeled_frames("./vid0_frames", labels, "./test_output")
+frames_to_video("./test_output", "./videos/test_labeled.mp4") """
